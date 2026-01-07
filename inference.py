d9543da5e419f30944c8b3d1175b1d447066b2c0
James W <22258235+wujameszj@users.noreply.github.com>
2026-01-07 08:10:37 +0000

    multi-file inference

--- inference.py
+++ inference.py
@@ -21,8 +21,12 @@ text_param = {
 }
 
 
-def run(messages: str):
+def run(video_param: dict, text_param: dict) -> str:
 
+    messages = [{
+        "role": "user",
+        "content": [video_param, text_param]
+    }]
     text = processor.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     images, videos, video_kwargs = process_vision_info(messages, image_patch_size=16, return_video_kwargs=True, return_video_metadata=True)
 
@@ -44,7 +48,7 @@ def run(messages: str):
     output_text = processor.batch_decode(
         generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
     )
-    return output_text[0] 
+    return output_text[0] + "\n\n"
 
 
 if __name__ == "__main__":
@@ -53,25 +57,29 @@ if __name__ == "__main__":
     ap.add_argument("--model", type=str, default="Qwen/Qwen3-VL-2B-Instruct")
     ap.add_argument("--prompt", type=str, default="Describe the person's emotional or mental state or changes therein.")
     ap.add_argument("--config", type=str, default="")
-    ap.add_argument("--out_path", type=str, default="out.txt")
+    ap.add_argument("-o", "--out_path", type=str, default="out.txt")
     args = ap.parse_args()
     
     t = time()
     processor = AutoProcessor.from_pretrained(args.model)
     model = AutoModelForImageTextToText.from_pretrained(
         args.model, dtype=bfloat16, 
-        attn_implementation="flash_attention_2", 
+#        attn_implementation="flash_attention_2", 
         device_map="auto")
     
     print("Model loaded in", int(time()-t), "seconds");  t = time()
 
+    text_param["text"] = args.prompt
     input_path = Path(args.input)
-    if input_path.is_file():
-        video_param["video"] = args.input
-        text_param["text"] = args.prompt
-        msg = {
-            "role": "user",
-            "content": [video_param, text_param]
-        }
-        print(run([msg]))
-        print(int(time()-t), "seconds elapsed.")
+    out = ""
+
+    filepaths = sorted(input_path.glob("*.mp4")) if input_path.is_dir() else [input_path]
+    for f in filepaths:
+        video_param["video"] = str(f)
+        res = f"{f.stem}:  " + run(video_param, text_param)
+        out += res
+        print(res)
+
+    print(f"{len(filepaths)} files processed in {int(time()-t)} seconds.")
+    with open(args.out_path, "w") as f:
+        f.write(out)
